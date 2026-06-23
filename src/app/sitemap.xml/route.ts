8c532d567ed48766c2757973daa13bf3a85a9ee9
ZzomB <mrjbk77@gmail.com>
2026-06-23 23:10:42 +0900

    root integration v2

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>https://www.wedodare.com/sitemap-root.xml</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+  </sitemap>
+  <sitemap>
+    <loc>https://www.wedodare.com/blog/sitemap.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+  return new NextResponse(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
